63238e6e0090e1a742eecaceb1361f8e442422f3
keya-desai <keyadesai97@gmail.com>
2020-03-29 18:08:51 -0400

    update to access csv locally

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,6 @@
 from flask import render_template
-from flask import jsonify
-
 from app import app
 from app.models import DataBase
-# from app import db
 from app import session
 from app import connection
 import pandas as pd
@@ -25,17 +22,13 @@ def home():
 
 @app.route('/worldmap')
 def worldmap():
-    # df = pd.read_sql("select eventid, country_txt, region_txt, latitude, longitude, b.num_attacks from main as a "
-    #                  "inner join "
-    #                  "(select count(country_txt) as num_attacks, country_txt as country from main group by country_txt ) as b"
-    #                  " on a.country_txt = b.country", connection)
-    df = pd.read_sql("select count(country_txt) as num_attacks, country_txt as country from main group by country_txt", connection)
-    data_json  = df.to_json()
-    # df.to_csv('data.csv')
-    print(data_json)
-    pdb.set_trace()
-    return render_template('worldmap_trial.html', title = 'World map', data_json = data_json)
+    return render_template('worldmap_trial.html', title = 'World map')
     # return render_template('worldmap.html', title='World map')
+
+@app.route('/get_csv_data')
+def get_csv_data():
+    df = pd.read_sql("select count(country_txt) as num_attacks, country_txt as name from main group by country_txt", connection)
+    return df.to_csv(index = False)
 
 
 
@@ -61,11 +54,6 @@ def index():
     line = create_plot_attack_per_year(connection)
     pie = create_plot_pie(connection)
     return render_template('index.html', title='Global Terrorism Analysis', plot1 = pie, plot2 = line)
-    # return render_template('layout.html', title = 'test', description = 'blah')
-
-
-# def new_page():
-#     return render_template('new_page.html')
 
 def create_plot():
 
@@ -93,9 +81,6 @@ def create_plot_attack_per_year(connection):
     df_new = df.groupby(['country_txt', 'iyear'], as_index=False).count().rename(columns={"eventid":"Occurence"})
     df_india = df_new.query("country_txt == 'India' ")
     df_usa = df_new.query("country_txt == 'United States' ")
-    # print(df_india)
-    # print(df_new)
-    print('-'*79)
 
     trace0 = go.Scatter(x = df_india['iyear'],
                         y = df_india['Occurence'],
@@ -113,7 +98,6 @@ def create_plot_attack_per_year(connection):
                         # title='Number of attacks per each year'
                         # line_color=df_usa['country_txt']
                         )
-    # layout = go.Layout(title = "test")
     data = [trace0,trace1]
 
     graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
@@ -124,14 +108,10 @@ def create_plot_attack_per_year(connection):
 def create_plot_pie(connection):
     df_pie = pd.read_sql("select eventid, attacktype1_txt from main where success = '1'", connection)   
     df_new = df_pie.groupby(['attacktype1_txt'], as_index=False).count().rename(columns={"eventid": "Occurence"})
-    # print(df_new)
-    # print('-' * 79)
-
     data = [
         go.Pie(
             values = df_new['Occurence'],
             labels = df_new['attacktype1_txt']
-            # title='Distribution of attack types which were successful'
         )
     ]
 
