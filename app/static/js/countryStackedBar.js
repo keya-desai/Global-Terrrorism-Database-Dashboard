2811130044b21c6fb28b51a3a40543546751f3f9
keya-desai <keyadesai97@gmail.com>
2020-04-07 20:57:40 -0400

    Added donut charts filtered by countries

--- app/static/js/countryStackedBar.js
+++ app/static/js/countryStackedBar.js
@@ -10,30 +10,73 @@
  * ---------------------------------------
  */
 
-countryList = ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bhutan', 'Bolivia', 'Bosnia-Herzegovina', 'Botswana', 'Brazil', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Canada', 'Central African Republic', 'Chad', 'Chile', 'China', 'Colombia', 'Comoros', 'Costa Rica', 'Croatia', 'Cuba', 'Cyprus', 'Czech Republic', 'Czechoslovakia', 'Democratic Republic of the Congo', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'East Germany (GDR)', 'East Timor', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Eritrea', 'Estonia', 'Ethiopia', 'Falkland Islands', 'Fiji', 'Finland', 'France', 'French Guiana', 'French Polynesia', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Greece', 'Grenada', 'Guadeloupe', 'Guatemala', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'International', 'Iran', 'Iraq', 'Ireland', 'Israel', 'Italy', 'Ivory Coast', 'Jamaica', 'Japan', 'Jordan', 'Kazakhstan', 'Kenya', 'Kosovo', 'Kuwait', 'Kyrgyzstan', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Lithuania', 'Luxembourg', 'Macau', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Martinique', 'Mauritania', 'Mauritius', 'Mexico', 'Moldova', 'Montenegro', 'Morocco', 'Mozambique', 'Myanmar', 'Namibia', 'Nepal', 'Netherlands', 'New Caledonia', 'New Hebrides', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'North Korea', 'North Yemen', 'Norway', 'Pakistan', 'Panama', 'Papua New Guinea', 'Paraguay', "People's Republic of the Congo", 'Peru', 'Philippines', 'Poland', 'Portugal', 'Qatar', 'Republic of the Congo', 'Rhodesia', 'Romania', 'Russia', 'Rwanda', 'Saudi Arabia', 'Senegal', 'Serbia', 'Serbia-Montenegro', 'Seychelles', 'Sierra Leone', 'Singapore', 'Slovak Republic', 'Slovenia', 'Solomon Islands', 'Somalia', 'South Africa', 'South Korea', 'South Sudan', 'South Vietnam', 'South Yemen', 'Soviet Union', 'Spain', 'Sri Lanka', 'St. Kitts and Nevis', 'St. Lucia', 'Sudan', 'Suriname', 'Swaziland', 'Sweden', 'Switzerland', 'Syria', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', 'Togo', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'Uruguay', 'Uzbekistan', 'Vanuatu', 'Vatican City', 'Venezuela', 'Vietnam', 'Wallis and Futuna', 'West Bank and Gaza Strip', 'West Germany (FRG)', 'Western Sahara', 'Yemen', 'Yugoslavia', 'Zaire', 'Zambia', 'Zimbabwe']
 
-
-var updatedropdown = function(country){
-    console.log(country)
-    data_list = []
+var updateLinePlot = function(country){
+    // console.log(country)
+    data_list_line = []
     d3.csv('/get_csv_data_lineplot/' + country, function(error, csvdata){
 
       csvdata.forEach(function(d){
-        data_list.push({"year" : d.iyear, "attacks": d.attacks, "kills": d.kills, "wounds": d.wounds});
+        data_list_line.push({"year" : d.iyear, "attacks": d.attacks, "kills": d.kills, "wounds": d.wounds});
       })
     // console.log(data);
-    amcharts(data_list, country);
+    amcharts_line(data_list_line, country);
   });
 }
+
+var updateAttackDonut = function(country){
+    console.log(country)
+    data_list_attack = []
+    d3.csv('/get_csv_attack_donutchart/' + country, function(error, attackdata){
+
+      attackdata.forEach(function(d){
+        data_list_attack.push({"type" : d.attacktype1_txt, "count" : d.count});
+      })
+    console.log(data_list_attack);
+    amcharts_attack(data_list_attack, country);
+  });
+}
+
+var updateTargetDonut = function(country){
+    // console.log(country)
+    data_list_target = []
+    d3.csv('/get_csv_target_donutchart/' + country, function(error, targetdata){
+
+      targetdata.forEach(function(d){
+        data_list_target.push({"type" : d.targtype1_txt, "count" : d.count});
+      })
+    // console.log(data);
+    amcharts_target(data_list_target, country);
+  });
+}
+
+var updateWeaponDonut = function(country){
+    // console.log(country)
+    data_list_weapon = []
+    d3.csv('/get_csv_weapon_donutchart/' + country, function(error, weapondata){
+
+      weapondata.forEach(function(d){
+        data_list_weapon.push({"type" : d.weaptype1_txt, "count" : d.count});
+      })
+    // console.log(data);
+    amcharts_weapon(data_list_weapon, country);
+  });
+}
+
 var dropdownChange = function(){
 
   country = d3.select(this).property('value')
-  updatedropdown(country)
+  updateLinePlot(country)
+  updateAttackDonut(country)
+  updateTargetDonut(country)
+  updateWeaponDonut(country)
 
 }
 
+countryList = ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bhutan', 'Bolivia', 'Bosnia-Herzegovina', 'Botswana', 'Brazil', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Canada', 'Central African Republic', 'Chad', 'Chile', 'China', 'Colombia', 'Comoros', 'Costa Rica', 'Croatia', 'Cuba', 'Cyprus', 'Czech Republic', 'Czechoslovakia', 'Democratic Republic of the Congo', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'East Germany (GDR)', 'East Timor', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Eritrea', 'Estonia', 'Ethiopia', 'Falkland Islands', 'Fiji', 'Finland', 'France', 'French Guiana', 'French Polynesia', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Greece', 'Grenada', 'Guadeloupe', 'Guatemala', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'International', 'Iran', 'Iraq', 'Ireland', 'Israel', 'Italy', 'Ivory Coast', 'Jamaica', 'Japan', 'Jordan', 'Kazakhstan', 'Kenya', 'Kosovo', 'Kuwait', 'Kyrgyzstan', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Lithuania', 'Luxembourg', 'Macau', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Martinique', 'Mauritania', 'Mauritius', 'Mexico', 'Moldova', 'Montenegro', 'Morocco', 'Mozambique', 'Myanmar', 'Namibia', 'Nepal', 'Netherlands', 'New Caledonia', 'New Hebrides', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'North Korea', 'North Yemen', 'Norway', 'Pakistan', 'Panama', 'Papua New Guinea', 'Paraguay', "People's Republic of the Congo", 'Peru', 'Philippines', 'Poland', 'Portugal', 'Qatar', 'Republic of the Congo', 'Rhodesia', 'Romania', 'Russia', 'Rwanda', 'Saudi Arabia', 'Senegal', 'Serbia', 'Serbia-Montenegro', 'Seychelles', 'Sierra Leone', 'Singapore', 'Slovak Republic', 'Slovenia', 'Solomon Islands', 'Somalia', 'South Africa', 'South Korea', 'South Sudan', 'South Vietnam', 'South Yemen', 'Soviet Union', 'Spain', 'Sri Lanka', 'St. Kitts and Nevis', 'St. Lucia', 'Sudan', 'Suriname', 'Swaziland', 'Sweden', 'Switzerland', 'Syria', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', 'Togo', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'Uruguay', 'Uzbekistan', 'Vanuatu', 'Vatican City', 'Venezuela', 'Vietnam', 'Wallis and Futuna', 'West Bank and Gaza Strip', 'West Germany (FRG)', 'Western Sahara', 'Yemen', 'Yugoslavia', 'Zaire', 'Zambia', 'Zimbabwe']
 var dropdown = d3.select("#countryButton")
                   .on("change", dropdownChange);
+
 
 dropdown.selectAll('myOptions')
       .data(countryList)
@@ -41,70 +84,20 @@ dropdown.selectAll('myOptions')
       .append('option')
       .text(function (d) { return d; }) 
       .attr("value", function (d) { return d; })
-      .property("selected", function(d){ return d == 'United States'; }); 
+      // .property("selected", function(d){ return d == 'United States'; }); 
 
-updatedropdown('United States')
+// updateLinePlot('United States')
+// updateAttackDonut('United States')
 
 
-
-function amcharts(data_list, country){
+function amcharts_line(data_list, country){
       am4core.useTheme(am4themes_animated);
 
     // Create chart instance
     var chart = am4core.create("stackedDiv", am4charts.XYChart);
 
-    console.log(data_list);
+    // console.log(data_list);
     chart.data = data_list;
-    // Add data
-    // chart.data = [
-    // {"year":  "1970", "attacks": 468, "kills": 33, "wounds": 160}, 
-    // {"year":  "1971", "attacks": 247, "kills": 20, "wounds": 55}, 
-    // {"year":  "1972", "attacks": 68, "kills": 10, "wounds": 35}, 
-    // {"year":  "1973", "attacks": 58, "kills": 45, "wounds": 33}, 
-    // {"year":  "1974", "attacks": 94, "kills": 16, "wounds": 54}, 
-    // {"year":  "1975", "attacks": 149, "kills": 24, "wounds": 158}, 
-    // {"year":  "1976", "attacks": 105, "kills": 4, "wounds": 41}, 
-    // {"year":  "1977", "attacks": 130, "kills": 4, "wounds": 17}, 
-    // {"year":  "1978", "attacks": 87, "kills": 8, "wounds": 8}, 
-    // {"year":  "1979", "attacks": 69, "kills": 18, "wounds": 58}, 
-    // {"year":  "1980", "attacks": 67, "kills": 15, "wounds": 22}, 
-    // {"year":  "1981", "attacks": 74, "kills": 8, "wounds": 15}, 
-    // {"year":  "1982", "attacks": 78, "kills": 11, "wounds": 37}, 
-    // {"year":  "1983", "attacks": 44, "kills": 8, "wounds": 5}, 
-    // {"year":  "1984", "attacks": 63, "kills": 3, "wounds": 780}, 
-    // {"year":  "1985", "attacks": 40, "kills": 3, "wounds": 13}, 
-    // {"year":  "1986", "attacks": 49, "kills": 1, "wounds": 36}, 
-    // {"year":  "1987", "attacks": 34, "kills": 1, "wounds": 1}, 
-    // {"year":  "1988", "attacks": 27, "kills": 1, "wounds": 1}, 
-    // {"year":  "1989", "attacks": 42, "kills": 3, "wounds": 14}, 
-    // {"year":  "1990", "attacks": 32, "kills": 5, "wounds": 7}, 
-    // {"year":  "1991", "attacks": 30, "kills": 2, "wounds": 4}, 
-    // {"year":  "1992", "attacks": 32, "kills": 2, "wounds": 3}, 
-    // {"year":  "1993", "attacks": 55, "kills": 10, "wounds": 16}, 
-    // {"year":  "1994", "attacks": 60, "kills": 178, "wounds": 738}, 
-    // {"year":  "1995", "attacks": 35, "kills": 2, "wounds": 119}, 
-    // {"year":  "1996", "attacks": 40, "kills": 2, "wounds": 18}, 
-    // {"year":  "1997", "attacks": 31, "kills": 4, "wounds": 3}, 
-    // {"year":  "1998", "attacks": 53, "kills": 20, "wounds": 40}, 
-    // {"year":  "1999", "attacks": 32, "kills": 0, "wounds": 7}, 
-    // {"year":  "2000", "attacks": 41, "kills": 3008, "wounds": 16515}, 
-    // {"year":  "2001", "attacks": 33, "kills": 4, "wounds": 11}, 
-    // {"year":  "2002", "attacks": 33, "kills": 0, "wounds": 0}, 
-    // {"year":  "2003", "attacks": 9, "kills": 0, "wounds": 0}, 
-    // {"year":  "2004", "attacks": 21, "kills": 0, "wounds": 0}, 
-    // {"year":  "2005", "attacks": 6, "kills": 1, "wounds": 14}, 
-    // {"year":  "2006", "attacks": 8, "kills": 0, "wounds": 0}, 
-    // {"year":  "2007", "attacks": 18, "kills": 2, "wounds": 13}, 
-    // {"year":  "2008", "attacks": 11, "kills": 18, "wounds": 41}, 
-    // {"year":  "2009", "attacks": 17, "kills": 4, "wounds": 17}, 
-    // {"year":  "2010", "attacks": 10, "kills": 0, "wounds": 2}, 
-    // {"year":  "2011", "attacks": 20, "kills": 7, "wounds": 7}, 
-    // {"year":  "2012", "attacks": 20, "kills": 23, "wounds": 436}, 
-    // {"year":  "2013", "attacks": 29, "kills": 26, "wounds": 19}, 
-    // {"year":  "2014", "attacks": 38, "kills": 54, "wounds": 58}, 
-    // {"year":  "2015", "attacks": 64, "kills": 68, "wounds": 139}, 
-    // {"year":  "2016", "attacks": 65, "kills": 95, "wounds": 932}
-    // ];
 
     // Create axes
     var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
@@ -169,4 +162,154 @@ function amcharts(data_list, country){
     chart.legend = new am4charts.Legend();
 
 }
-// Themes begin
+
+
+function amcharts_attack(data_list, country){
+
+  // Themes begin
+  am4core.useTheme(am4themes_animated);
+  // Themes end
+  // Create chart instance
+  var chart = am4core.create("attackDonutDiv", am4charts.PieChart);
+
+  // Add data
+  chart.data = data_list;
+  console.log(data_list)
+  // chart.data = [
+  // {"type":  "Armed Assault", "count": 284}, 
+  // {"type":  "Unarmed Assault", "count": 61}, 
+  // {"type":  "Assassination", "count": 133}, 
+  // {"type":  "Bombing/Explosion", "count": 1383}, 
+  // {"type":  "Facility/Infrastructure Attack", "count": 864}, 
+  // {"type":  "Hijacking", "count": 17}, 
+  // {"type":  "Hostage Taking (Barricade Incident)", "count": 62}, 
+  // {"type":  "Hostage Taking (Kidnapping)", "count": 21}, 
+  // {"type":  "Unknown", "count": 11}
+  // ];
+
+  // Set inner radius
+  chart.innerRadius = am4core.percent(50);
+
+  var title = chart.titles.create();
+  title.text = "Attack Type Distribution in "+ country;
+  title.fontSize = 20;
+  title.marginBottom = 10;
+
+  /*  var label = chart.chartContainer.createChild(am4core.Label);
+   label.text = "Target Type Distribution in the U.S.";
+   label.align = "center"; */
+
+  // Add and configure Series
+  var pieSeries = chart.series.push(new am4charts.PieSeries());
+  pieSeries.dataFields.value = "count";
+  pieSeries.dataFields.category = "type";
+  pieSeries.slices.template.stroke = am4core.color("#fff");
+  pieSeries.slices.template.strokeWidth = 2;
+  pieSeries.slices.template.strokeOpacity = 1;
+
+  // This creates initial animation
+  pieSeries.hiddenState.properties.opacity = 1;
+  pieSeries.hiddenState.properties.endAngle = -90;
+  pieSeries.hiddenState.properties.startAngle = -90;
+
+}
+
+
+function amcharts_target(data_list, country){
+
+  // Themes begin
+  am4core.useTheme(am4themes_animated);
+  // Themes end
+  // Create chart instance
+  var chart = am4core.create("targetDonutDiv", am4charts.PieChart);
+
+  // Add data
+  chart.data = data_list;
+  // chart.data = [
+  // {"type":  "Armed Assault", "count": 284}, 
+  // {"type":  "Unarmed Assault", "count": 61}, 
+  // {"type":  "Assassination", "count": 133}, 
+  // {"type":  "Bombing/Explosion", "count": 1383}, 
+  // {"type":  "Facility/Infrastructure Attack", "count": 864}, 
+  // {"type":  "Hijacking", "count": 17}, 
+  // {"type":  "Hostage Taking (Barricade Incident)", "count": 62}, 
+  // {"type":  "Hostage Taking (Kidnapping)", "count": 21}, 
+  // {"type":  "Unknown", "count": 11}
+  // ];
+
+  // Set inner radius
+  chart.innerRadius = am4core.percent(50);
+
+  var title = chart.titles.create();
+  title.text = "Target Type Distribution in "+ country;
+  title.fontSize = 20;
+  title.marginBottom = 10;
+
+  /*  var label = chart.chartContainer.createChild(am4core.Label);
+   label.text = "Target Type Distribution in the U.S.";
+   label.align = "center"; */
+
+  // Add and configure Series
+  var pieSeries = chart.series.push(new am4charts.PieSeries());
+  pieSeries.dataFields.value = "count";
+  pieSeries.dataFields.category = "type";
+  pieSeries.slices.template.stroke = am4core.color("#fff");
+  pieSeries.slices.template.strokeWidth = 2;
+  pieSeries.slices.template.strokeOpacity = 1;
+
+  // This creates initial animation
+  pieSeries.hiddenState.properties.opacity = 1;
+  pieSeries.hiddenState.properties.endAngle = -90;
+  pieSeries.hiddenState.properties.startAngle = -90;
+
+}
+
+
+function amcharts_weapon(data_list, country){
+
+  // Themes begin
+  am4core.useTheme(am4themes_animated);
+  // Themes end
+  // Create chart instance
+  var chart = am4core.create("weaponDonutDiv", am4charts.PieChart);
+
+  // Add data
+  chart.data = data_list;
+  // chart.data = [
+  // {"type":  "Armed Assault", "count": 284}, 
+  // {"type":  "Unarmed Assault", "count": 61}, 
+  // {"type":  "Assassination", "count": 133}, 
+  // {"type":  "Bombing/Explosion", "count": 1383}, 
+  // {"type":  "Facility/Infrastructure Attack", "count": 864}, 
+  // {"type":  "Hijacking", "count": 17}, 
+  // {"type":  "Hostage Taking (Barricade Incident)", "count": 62}, 
+  // {"type":  "Hostage Taking (Kidnapping)", "count": 21}, 
+  // {"type":  "Unknown", "count": 11}
+  // ];
+
+  // Set inner radius
+  chart.innerRadius = am4core.percent(50);
+
+  var title = chart.titles.create();
+  title.text = "Weapon Type Distribution in "+ country;
+  title.fontSize = 20;
+  title.marginBottom = 10;
+
+  /*  var label = chart.chartContainer.createChild(am4core.Label);
+   label.text = "Target Type Distribution in the U.S.";
+   label.align = "center"; */
+
+  // Add and configure Series
+  var pieSeries = chart.series.push(new am4charts.PieSeries());
+  pieSeries.dataFields.value = "count";
+  pieSeries.dataFields.category = "type";
+  pieSeries.slices.template.stroke = am4core.color("#fff");
+  pieSeries.slices.template.strokeWidth = 2;
+  pieSeries.slices.template.strokeOpacity = 1;
+
+  // This creates initial animation
+  pieSeries.hiddenState.properties.opacity = 1;
+  pieSeries.hiddenState.properties.endAngle = -90;
+  pieSeries.hiddenState.properties.startAngle = -90;
+
+}
